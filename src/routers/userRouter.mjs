489bd44d80f5682ad535180e6df51a83b41c9982
Nikita Tkachev <nikita.tkachev.dev@gmail.com>
2023-08-27 10:44:18 +0300

    Add controllers

--- src/routers/userRouter.mjs
+++ src/routers/userRouter.mjs
@@ -1,12 +1,19 @@
 import express from 'express';
 
 import routes from '../routes.mjs';
+import {
+    changePassword,
+    editProfile,
+    userDetail,
+    users,
+} from '../controllers/userController.mjs';
 
 
 const userRouter = express.Router();
 
-userRouter.get(routes.home, (req, res) => res.send('user index'));
-userRouter.get('/edit', (req, res) => res.send('user edit'));
-userRouter.get('/profile', (req, res) => res.send('user index'));
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
--- src/routers/videoRouter.mjs
+++ src/routers/videoRouter.mjs
@@ -1,5 +1,21 @@
 import express from 'express';
 
+import routes from '../routes.mjs';
+import {
+    upload,
+    videoDelete,
+    videoDetail,
+    videoEdit,
+    videos,
+} from '../controllers/videoController.mjs';
+
+
 export const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoEdit, videoEdit);
+videoRouter.get(routes.videoDelete, videoDelete);
 
 export default videoRouter;

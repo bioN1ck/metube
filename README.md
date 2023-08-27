2b8c2b1f08a7a026ebdae0373c09e113e723e2e1
Nikita Tkachev <nikita.tkachev.dev@gmail.com>
2023-08-27 19:28:04 +0300

    Fill pages with some content

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ## MyTube
 
 Clone of YouTube backend service
+
+### Pages
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- src/controllers/videoController.mjs
+++ src/controllers/videoController.mjs
@@ -1,5 +1,8 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const search = (req, res) => res.render('search', { pageTitle: 'Search' });
+export const search = (req, res) => {
+    const { query: searchQuery } = req.query
+    res.render('search', { pageTitle: 'Search', searchQuery });
+}
 export const videos = (req, res) => res.render('videos', { pageTitle: 'Videos' });
 export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
 export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
